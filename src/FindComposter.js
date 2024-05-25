b2b5eb0a985be8b59922b0afc6675dff8c9f9ac2
Lyra126 <ccclarissa126@gmail.com>
2024-05-24 17:20:25 -0700

    added find composter main page

--- src/FindComposter.js
+++ src/FindComposter.js
@@ -1,16 +1,59 @@
-import React, { useRef, useEffect, useState } from "react";
-import { View, Text, Image, StyleSheet, TouchableOpacity, Modal, ImageBackground} from "react-native";
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from "react-native";
 
-const FindComposter = () => {
-    return (
-        <Text >"Hello World"</Text>
-    );
+const FindComposter = ({ navigation }) => {
+  const navigateToScreen = (screen) => {
+    navigation.navigate(screen);
+  };
+
+  return (
+    <ImageBackground source={require("./assets/fc/fc.png")} style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Find a Composter Near You</Text>
+        <TouchableOpacity style={styles.button} onPress={() => navigateToScreen("Map")}>
+          <Text style={styles.buttonText}>Farm</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigateToScreen("Map")}>
+          <Text style={styles.buttonText}>Community Garden</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigateToScreen("Map")}>
+          <Text style={styles.buttonText}>Farmer Market</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    }
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    marginBottom: 300
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "black",
+  },
+  button: {
+    backgroundColor: "#26a69a",
+    padding: 16,
+    borderRadius: 8,
+    marginVertical: 10,
+    width: '80%',
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+  },
 });
 
 export default FindComposter;
