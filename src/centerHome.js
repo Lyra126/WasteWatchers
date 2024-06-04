49097b1a7a3e15602c0ea225f28466e87eb4ef67
Lyra126 <ccclarissa126@gmail.com>
2024-06-04 16:54:35 -0700

    added open in maps for saved locations

--- src/centerHome.js
+++ src/centerHome.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
-import { ScrollView, View, Animated, Image, StyleSheet, SafeAreaView, Text, TouchableOpacity, Modal} from "react-native";
+import { ScrollView, View, Animated, Image, Linking,StyleSheet, SafeAreaView, Text, TouchableOpacity,Platform, Modal} from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import globalStyles from "./styles/globalStyles";
 import axios from "axios"; // Don't forget to import axios
@@ -47,6 +47,19 @@ const CenterHome = ({route}) => {
         setShowSavedLocationsPopup(!showSavedLocationsPopup);
     };
 
+    const navigateToAppleMaps = (currentAddress) => {
+        const scheme = Platform.select({
+            ios: 'maps://',
+        });
+    
+        const url = Platform.select({
+            ios: `${scheme}?q=${encodeURIComponent(currentAddress)}`,
+        });
+    
+        Linking.openURL(url);
+    };
+
+
     return (
         <SafeAreaView style={[globalStyles.AndroidSafeArea, styles.container]}>
             <ScrollView contentContainerStyle={styles.scrollContainer}>
@@ -74,7 +87,12 @@ const CenterHome = ({route}) => {
                         <View style={styles.modalContainer}>
                             <View style={styles.modalContent}>
                                 {savedLocations.map((location, index) => (
-                                    <Text key={index}>{location}</Text>
+                                    <View key={index}>
+                                        <Text style = {styles.locationText}>{index+1}: {location}</Text>
+                                        <TouchableOpacity style={styles.buttonContainer} onPress={() => navigateToAppleMaps(location)}>
+                                            <Text style={styles.buttonLabel}>►</Text>
+                                        </TouchableOpacity>
+                                    </View>
                                 ))}
                                 <TouchableOpacity onPress={toggleSavedLocationsPopup}>
                                     <Text style={styles.closeButton}>Close</Text>
@@ -89,7 +107,8 @@ const CenterHome = ({route}) => {
             <View style = {styles.treePreviewView}>
                 <Image
                     style={styles.treeImage}
-                    source={require('./assets/fruitTrees/apple1.png')}/>
+                    source={require('./assets/fruitTrees/apple5.png')}
+                    />
                 <View style={styles.treePreviewText}>
                     <Text style = {{fontWeight: 'bold', fontSize: '26px', color: '#e7e7e7'}}>Apple Tree</Text>
                     <Text style = {{color: '#e7e7e7'}}>13 weeks old</Text>
@@ -123,7 +142,7 @@ const styles = StyleSheet.create({
         borderRadius: 50,
     },
     treePreviewView: {
-        height: 400,  // Adjust height if necessary
+        height: 650,  // Adjust height if necessary
         justifyContent: 'space-between',
         padding: '4.5%',
         backgroundColor: '#5f7046',
@@ -147,7 +166,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#5f7046',
         borderRadius: 30,
         flexDirection: 'column',
-        height: 150,
+        height: 110,
         justifyContent: "center",
         alignItems: "center",
         
@@ -168,6 +187,7 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "rgba(0, 0, 0, 0.5)",
+        
     },
     modalContent: {
         backgroundColor: "white",
@@ -175,6 +195,8 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         alignItems: "center",
         elevation: 5,
+        width: '70%',
+    
     },
     homeButton: {
         backgroundColor: "#EE856F",
@@ -190,6 +212,21 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         justifyContent: "center",
         textAlign: "center"
+    },
+    buttonContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 8,
+        backgroundColor: "#26f",
+        borderRadius: 8,
+        width:40
+    },
+    buttonLabel: {
+        fontSize: 18,
+        color: "white",
+    },
+    locationText: {
+        fontSize:20
     }
 });
 
